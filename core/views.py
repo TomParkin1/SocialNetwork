cc4a9642629e9cb3b9dca0db340d000e40613740
Tom P <tom.was.taken.was.taken@gmail.com>
2024-02-05 15:01:37 +0000

    sign up page worked on

--- core/views.py
+++ core/views.py
@@ -5,3 +5,14 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def signup(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+    else:
+        return render(request, 'signup.html')
